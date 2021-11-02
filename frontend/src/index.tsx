8c62f9e028e003ba37b0eb7c6724ea2bfb24699a
Rebeca <rebecasaarcos@gmail.com>
2021-11-02 15:09:46 -0300

    Basic static components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/css/styles.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(
